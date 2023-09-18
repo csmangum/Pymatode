8a5ed949524a8eaad1cf97e6c47e3e3a7027cdf7
Chris Mangum <csmangum@gmail.com>
2023-09-18 11:25:03 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+"""
+The ASH neurons in the nematode Caenorhabditis elegans (C. elegans) are sensory neurons that play a significant role in the worm's avoidance behaviors. These neurons are sensitive to a range of environmental stimuli, such as noxious chemicals, high osmolarity, and mechanical forces. When the ASH neurons detect these adverse conditions, they trigger an avoidance response, usually causing the worm to change direction or back away.
 
+The ASH neurons are bilaterally symmetric, meaning there's one on each side of the worm's head. They are part of a larger sensory network that includes other types of neurons, such as AWA and ADF, which are involved in attraction behaviors. The ASH neurons send signals to interneurons and motor neurons to bring about a coordinated response to the detected stimuli.
+
+Because of their involvement in avoidance behavior, the ASH neurons have been the subject of extensive research, often serving as a model for understanding how sensory neurons encode and process information in a simple nervous system.
+
+Overall, the ASH neurons provide a key function in the worm's ability to navigate its environment by avoiding harmful conditions.
+"""
