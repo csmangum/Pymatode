5203e088a6aa141da2f50a16eef8ef3fa6473e56
Chris Mangum <csmangum@gmail.com>
2023-09-19 13:37:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,61 @@ Because of their involvement in avoidance behavior, the ASH neurons have been th
 
 Overall, the ASH neurons provide a key function in the worm's ability to navigate its environment by avoiding harmful conditions.
 """
+
+class Neuron:
+    def __init__(self, name):
+        self.name = name
+        self.downstream = []
+        self.state = 0  # or other initial state
+
+ASH = Neuron("ASH")
+AVB = Neuron("AVB")
+AVA = Neuron("AVA")
+AVD = Neuron("AVD")
+AVE = Neuron("AVE")
+
+ASH.downstream = [AVB, AVA, AVD, AVE]
+
+
+def activate(neuron):
+    # some activation function on neuron.state
+    neuron.state += 1  # Simplified example
+
+def send_signal(neuron):
+    for downstream_neuron in neuron.downstream:
+        # do something to update downstream_neuron.state
+        downstream_neuron.state += neuron.state  # Simplified example
+
+for timestep in range(100):  # Example with 100 timesteps
+    activate(ASH)
+    send_signal(ASH)
+
+def apply_input(neuron, input_value):
+    neuron.state += input_value
+
+class Circuit:
+    def __init__(self, name):
+        self.name = name
+        self.neurons = {}
+        self.connections = {}
+        
+    def add_neuron(self, neuron):
+        self.neurons[neuron.name] = neuron
+
+    def add_connection(self, from_neuron, to_neuron):
+        if from_neuron.name not in self.connections:
+            self.connections[from_neuron.name] = []
+        self.connections[from_neuron.name].append(to_neuron)
+
+
+
+common_circuit = Circuit("Common")
+common_circuit.add_neuron(ASH)
+# ASH can be reused in other circuits
+
+
+
+
+
+
+
